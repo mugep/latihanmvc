9ac53e27965ebb75dd3d6f413b0b1aa24e975838
Muhamad Gerald <geralptr21@gmail.com>
2020-11-25 16:16:27 +0700

    first commit

---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Gerald';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/muge.jpg" alt="Muge" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?> umur saya <?= $data['umur']; ?>, saya adalah <?= $data['pekerjaan']; ?></p>
+</div>
